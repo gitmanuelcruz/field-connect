edf12922b56caa648d090079e7c9ea6627249d83
Manuel <mcmanuelster@gmail.com>
2022-06-30 15:50:22 -0500

    Formularios

--- src/components/paginas/Inicio.jsx
+++ src/components/paginas/Inicio.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+
 
 export const Inicio = () => {
   return (
     <Container>
-    <Form>
-      <Form.Group controlId="formName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter name" />
-      </Form.Group>
-    </Form>
-  </Container>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   )
 }
